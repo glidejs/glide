2a88aa28fa25b51dc651d13fd29fa278cd3b8623
jedrzejchalubek <jedrzej.chalubek@gmail.com>
2017-11-19 03:01:44 +0100

    Fix test fixture

--- tests/fixtures/html.js
+++ tests/fixtures/html.js
@@ -1,11 +1,11 @@
 export default `
 <div id="glide" class="glide">
-  <div data-glide="arrows" class="glide__arrows">
+  <div data-glide-el="arrows" class="glide__arrows">
     <button class="glide__arrow prev" data-glide-dir="<">prev</button>
     <button class="glide__arrow next" data-glide-dir=">">next</button>
   </div>
 
-  <div data-glide="track" class="glide__track">
+  <div data-glide-el="track" class="glide__track">
     <ul class="glide__slides">
       <li class="glide__slide">1</li>
       <li class="glide__slide">2</li>
@@ -13,6 +13,6 @@ export default `
     </ul>
   </div>
 
-  <div data-glide="bullets" class="glide__bullets"></div>
+  <div data-glide-el="bullets" class="glide__bullets"></div>
 </div>
 `

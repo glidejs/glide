f63ae8cf7bada5657145ed5e53e308a7b1ce763e
jedrzejchalubek <jedrzej.chalubek@gmail.com>
2017-12-17 16:52:26 +0100

    Cleanups in init test

--- tests/unit/init.test.js
+++ tests/unit/init.test.js
@@ -1,15 +1,21 @@
 import { init } from '../../src/core/index'
 
 describe('`init()` function should', () => {
-  test('initialize all registered components with Glide instance and Components collection as an argument', () => {
+  test('initialize all registered components', () => {
+    // Here we creating a mock of glide internal component. 
+    // Every component have to implement a `init` method and be a function
     let fn = jest.fn()
-    
+    let stub = { init: fn }
     let mock = jest.fn(() => {
-      return { init: fn }
+      return stub
     })
 
     init('glide', { mock })
 
-    expect(mock).toHaveBeenCalledWith('glide', { mock: { init: fn } })
+    // `init()` method of the component should be called
+    expect(fn).toHaveBeenCalled()
+    
+    // component should be initialized with glide and other components as arguments
+    expect(mock).toHaveBeenCalledWith('glide', { mock: stub })
   })
 })

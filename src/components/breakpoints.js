920d19d36174afd868171ec83884d7572888a80d
jedrzejchalubek <jedrzej.chalubek@gmail.com>
2018-03-29 10:23:57 +0200

    For better performance bind direct resize listener instead of using instance resize event in breakpoints module

--- src/components/breakpoints.js
+++ src/components/breakpoints.js
@@ -1,6 +1,9 @@
 import { warn } from '../utils/log'
+import { throttle } from '../utils/wait'
 import { isObject } from '../utils/unit'
 import { sortKeys } from '../utils/object'
+
+import EventsBinder from '../core/event/events-binder'
 
 /**
  * Sorts keys of breakpoint object so they will be ordered from lower to bigger.
@@ -19,6 +22,13 @@ function sortBreakpoints (points) {
 }
 
 export default function (Glide, Components, Events) {
+  /**
+   * Instance of the binder for DOM Events.
+   *
+   * @type {EventsBinder}
+   */
+  const Binder = new EventsBinder()
+
   /**
    * Holds reference to settings.
    *
@@ -55,7 +65,7 @@ export default function (Glide, Components, Events) {
      */
     match (points) {
       if (typeof window.matchMedia !== 'undefined') {
-        for (const point in points) {
+        for (let point in points) {
           if (points.hasOwnProperty(point)) {
             if (window.matchMedia(`(max-width: ${point}px)`).matches) {
               return points[point]
@@ -78,9 +88,9 @@ export default function (Glide, Components, Events) {
    * Update glide with settings of matched brekpoint:
    * - window resize to update slider
    */
-  Events.on('resize', () => {
+  Binder.on('resize', window, throttle(() => {
     settings = Object.assign(settings, Breakpoints.match(points))
-  })
+  }, Glide.settings.throttle))
 
   /**
    * Resort and update default settings:
@@ -90,6 +100,14 @@ export default function (Glide, Components, Events) {
     points = sortBreakpoints(points)
 
     defaults = Object.assign({}, settings)
+  })
+
+  /**
+   * Unbind resize listener:
+   * - on destroying, to bring markup to its initial state
+   */
+  Events.on('destroy', () => {
+    Binder.off('resize', window)
   })
 
   return Breakpoints

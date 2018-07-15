9ba3485e8348e91c25ca8836ab473eb2fdfaffa0
Jędrzej Chałubek <jedrzej.chalubek@gmail.com>
2018-07-15 22:58:48 +0200

    feat(transformer): Allow for extending collection of translate mutators (#242)
    * Draft idea of extending mutators array
    
    * Introduce mutate() api method
    
    * Refactor how mutators array is concated
    
    * Return glide instance in mutate() api method

--- src/transformer/index.js
+++ src/transformer/index.js
@@ -5,29 +5,27 @@ import Peeking from './mutators/peeking'
 import Focusing from './mutators/focusing'
 
 /**
- * Collection of transformers.
- *
- * @type {Array}
- */
-const MUTATORS = [
-  Gap,
-  Grow,
-  Peeking,
-  Focusing,
-  // It's important that the Rtl component
-  // be last on the list, so it reflects
-  // all previous transformations.
-  Rtl
-]
-
-/**
  * Applies diffrent transformers on translate value.
  *
  * @param  {Object} Glide
  * @param  {Object} Components
  * @return {Object}
  */
-export default function (Glide, Components) {
+export default function (Glide, Components, Events) {
+  /**
+   * Merge instance mutators with collection of default transformers.
+   * It's important that the Rtl component be last on the list,
+   * so it reflects all previous transformations.
+   *
+   * @type {Array}
+   */
+  let MUTATORS = [
+    Gap,
+    Grow,
+    Peeking,
+    Focusing
+  ].concat(Glide._m, [Rtl])
+
   return {
     /**
      * Piplines translate value with registered transformers.
@@ -37,7 +35,7 @@ export default function (Glide, Components) {
      */
     mutate (translate) {
       for (var i = 0; i < MUTATORS.length; i++) {
-        translate = MUTATORS[i](Glide, Components).modify(translate)
+        translate = MUTATORS[i](Glide, Components, Events).modify(translate)
       }
 
       return translate

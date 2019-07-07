c79226d7005c24615967799a0fe46b46ca75dbde
Jędrzej Chałubek <jedrzej.chalubek@gmail.com>
2019-07-07 23:31:50 +0200

    test(): Fix quering of slides and clones

--- tests/fixtures/query.js
+++ tests/fixtures/query.js
@@ -1,5 +1,5 @@
 const ROOT_SELECTOR = '.glide'
-const CLONE_SELECTOR = '.glide__slide--clone'
+const CLONE_CLASS = 'glide__slide--clone'
 const TRACK_SELECTOR = '[data-glide-el="track"]'
 const BULLETS_SELECTOR = `[data-glide-dir*="="]`
 const PREVIOUS_CONTROLS_SELECTOR = `[data-glide-dir*="<"]`
@@ -14,10 +14,10 @@ export function query (document) {
   const previousControls = root.querySelectorAll(PREVIOUS_CONTROLS_SELECTOR)
   const nextControls = root.querySelectorAll(NEXT_CONTROLS_SELECTOR)
   const slides = Array.from(wrapper.children).filter((slide) => {
-    return !slide.classList.contains(CLONE_SELECTOR)
+    return !slide.classList.contains(CLONE_CLASS)
   })
   const clones = Array.from(wrapper.children).filter((slide) => {
-    return slide.classList.contains(CLONE_SELECTOR)
+    return slide.classList.contains(CLONE_CLASS)
   })
 
   return {

7dedba2b25cb6feb9fe23a55782d8d2a96e01cbb
Jędrzej Chałubek <jedrzej.chalubek@gmail.com>
2021-11-21 14:26:51 +0100

    chore: build dist

--- dist/glide.esm.js
+++ dist/glide.esm.js
@@ -734,6 +734,7 @@ var Glide$1 = /*#__PURE__*/function () {
 
       return this;
     }
+<<<<<<< HEAD
     /**
      * Change slide with specified pattern. A pattern must be in the special format:
      * `>` - Move one forward
@@ -744,6 +745,20 @@ var Glide$1 = /*#__PURE__*/function () {
      *
      * @param {String} pattern
      * @return {Glide}
+=======
+    /**
+     * Change slide with specified pattern. A pattern must be in the special format:
+     * `>` - Move one forward
+     * `<` - Move one backward
+     * `={i}` - Go to {i} zero-based slide (eq. '=1', will go to second slide)
+     * `>>` - Rewinds to end (last slide)
+     * `<<` - Rewinds to start (first slide)
+     * `|>` - Move one viewport forward
+     * `|<` - Move one viewport backward
+     *
+     * @param {String} pattern
+     * @return {Glide}
+>>>>>>> 4697d13 (chore: build dist)
      */
 
   }, {
--- dist/glide.js
+++ dist/glide.js
@@ -740,6 +740,7 @@
 
         return this;
       }
+<<<<<<< HEAD
       /**
        * Change slide with specified pattern. A pattern must be in the special format:
        * `>` - Move one forward
@@ -750,6 +751,20 @@
        *
        * @param {String} pattern
        * @return {Glide}
+=======
+      /**
+       * Change slide with specified pattern. A pattern must be in the special format:
+       * `>` - Move one forward
+       * `<` - Move one backward
+       * `={i}` - Go to {i} zero-based slide (eq. '=1', will go to second slide)
+       * `>>` - Rewinds to end (last slide)
+       * `<<` - Rewinds to start (first slide)
+       * `|>` - Move one viewport forward
+       * `|<` - Move one viewport backward
+       *
+       * @param {String} pattern
+       * @return {Glide}
+>>>>>>> 4697d13 (chore: build dist)
        */
 
     }, {
